8c42f5d69d6fd326f042f88eae5cf4dd207f8ab3
Greg Schuessler <GregSchuessler@Gregs-MacBook-Pro.local>
2017-03-25 14:03:36 -0500

    My first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//print(str)
+
+var myNumber = 1
+myNumber = myNumber+1
+myNumber = myNumber+1
+//test
